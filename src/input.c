a1c2d1510e81b3e7c2922c3ba28b282f1a846e81
Jorge Topete <jtopete@miners.utep.edu>
2021-02-12 11:48:11 -0700

    Implemented both non and space char methods. Morover, tried using pointer for the array but failed to do so. Nevertheless it is able to know what type of character it receives.

--- src/input.c
+++ src/input.c
@@ -2,18 +2,59 @@
 
 #define MAX_INPUT 100
 
+int get_input(int c, int input_arr[]);
+int space_char(char c);
+int non_space_char(char c);
+
 void main() {
   // Variables
-  char input[MAX_INPUT];
+  int input_arr[MAX_INPUT];
+  int input_pointer;
   int c;
-  // First way to ask for user input.
-  printf("Please enter a phrase: ");
-  fgets(input, 100, stdin);
-  printf("Phrase 1st way: %s", input);
   // Second to ask for user input.
-  printf("Please enter another phrase: ");
-  while ((c = getchar()) != '\n' && c != EOF){
-    putchar(c);
+  printf("Please enter a phrase: ");
+  input_pointer = get_input(c, input_arr);
+  /*
+  for (int i = 0; i < sizeof(input_arr); i++) {
+    printf("%c" + input_arr[i]);
   }
+  */
   printf("\n");
+}
+
+int get_input(int c, int input_arr[]) {
+  int checker;
+  int pointer = 0;
+  char letter;
+  while ((c = getchar()) != '\n' && c != EOF) {
+    input_arr[pointer] = c;
+    checker = space_char(input_arr[pointer]);
+    if (checker == 0) {
+      printf("Whitespace character: ");
+      putchar(c);
+      printf("\n");
+    }
+    checker = non_space_char(input_arr[pointer]);
+    if (checker == 0) {
+      printf("Non-whitespace character: ");
+      putchar(c);
+      printf("\n");
+    }
+    pointer++;
+  }
+  return 0;
+}
+
+int space_char(char c) {
+  if (c == ' ' || c == '\t' || c == '\n' && c != '\0') {
+    return 0;
+  }
+  return 1;
+}
+
+int non_space_char(char c) {
+  if (c != ' ' && c != '\t' && c != '\n' && c != '\0') {
+    return 0;
+  }
+  return 1;
 }
